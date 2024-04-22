9e6f9244580f30fc09a5e2e7f99e22d9a49fc331
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-04-22 17:43:17 +0200

    possibility to add favorite cities + adding style

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <div>
+      <h1 className="text-4xl font-bold text-center text-blue-700">Aunt May</h1>
         <WeatherApp />
       </div>
     </>
--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -7,6 +7,7 @@ const WeatherApp = () => {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
+  const [favorites, setFavorites] = useState([]);
 
   // récupération des datas du fetch
   const fetchWeatherData = async () => {
@@ -27,7 +28,20 @@ const WeatherApp = () => {
     }
   };
 
-  // vérifie que la ville recherchée n'est pas une string vide 
+  // ajoute la ville dans les favoris
+  const addToFavorites = () => {
+    if (weatherData && !favorites.includes(weatherData.name)) {
+      setFavorites([...favorites, weatherData.name]);
+    }
+  };
+
+  // recherche et fetch la ville favorite sélectionnée
+  const searchFavoriteWeather = (favoriteCity) => {
+    setCity(favoriteCity);
+    fetchWeatherData(favoriteCity);
+  };
+
+  // vérifie que la ville recherchée n'est pas une string vide
   const handleSearch = () => {
     if (city.trim() !== "") {
       fetchWeatherData();
@@ -37,24 +51,50 @@ const WeatherApp = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="Entrez le nom de la ville"
-      />
-      <button onClick={handleSearch}>Rechercher</button>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div>
+        <div className="flex items-center m-10">
+          <input
+            className="w-auto border border-gray-300 rounded-md py-2 px-4 mr-2 focus:outline-none focus:border-blue-500"
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Entrez le nom de la ville"
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            onClick={handleSearch}
+          >
+            Rechercher
+          </button>
+        </div>
+      </div>
+
       {error && <p style={{ color: "red" }}>{error}</p>}
       {weatherData && (
-        <div>
+        <div className="flex justify-center items-center flex-col">
           <h2>{weatherData.name}</h2>
           <p>Météo: {weatherData.weather[0].description}</p>
           <p>Température: {weatherData.main.temp}°C</p>
           <p>Ressenti: {weatherData.main.feels_like}°C</p>
           <p>Vent: {weatherData.wind.speed} km/h</p>
+          <button
+            className="text-2xl bg-blue-400 text-white px-4 py-2 shadow-md m-4"
+            type="button"
+            onClick={addToFavorites}
+          >
+            Ajouter aux favoris
+          </button>
         </div>
       )}
+      <h3>Favoris:</h3>
+      <ul>
+        {favorites.map((favorite, index) => (
+          <li key={index} onClick={() => searchFavoriteWeather(favorite)}>
+            {favorite}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
